c3e5b86dd9d68a31633cd7f1ad71026c249ed059
atf1206 <atf1206@gmail.com>
2016-07-08 20:47:51 -0400

    2nd indeed change

--- indeed.js
+++ indeed.js
@@ -9,7 +9,7 @@ function myGetIndeed(sourceArray)
     }).done(function(result) {
       $("#results").empty();
       for (var i = 0; i < 10; i++) {
-        //console.log(result["results"][i]["title"]);
+        console.log(result);
 		
         $("#results1").append("<div class='job'><a target='_blank' href='#' class='jobtitle'></a><span class='company_location'><span class='company'></span> - <span class='location'></span></span></div>");
         
